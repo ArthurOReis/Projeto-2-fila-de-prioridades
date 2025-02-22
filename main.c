dff3074a90be1af094475042e6fad6412baf4548
mvduarte <100307618+mvduarte@users.noreply.github.com>
2025-02-22 16:33:06 -0300

    libprg do edu

--- main.c
+++ main.c
@@ -1,7 +1,43 @@
+#include <stdbool.h>
 #include <stdio.h>
-// para usar a libprg, inclua librpg/libprg.h
+#include <stdlib.h>
+#include <string.h>
 #include <libprg/libprg.h>
 
-int main() {
-    printf("Teste");
+int main() {int quantidade[6];
+    char resposta;
+    apb_t * filaDeAtendimento = apb_cria("A001");
+    char senha[10];
+    while (true) {
+        printf(">");
+
+        scanf("%c",&resposta);
+        switch (resposta) {
+            case 'A':
+                quantidade[0]++;
+            if (quantidade[0]<10)
+                sprintf(senha,"A00%d",quantidade[0]);
+            else if (quantidade[0]<100)
+                sprintf(senha,"A0%d",quantidade[0]);
+            else
+                sprintf(senha,"A%d",quantidade[0]);
+            apb_adiciona(filaDeAtendimento,senha);
+            break;
+            case 'B':
+                break;
+            case 'C':
+                break;
+            case 'D':
+                break;
+            case 'E':
+                break;
+            case 'F':
+                break;
+            case '?':
+                break;
+            default:
+                return 0;
+        }
+    }
+    printf(apb_obtem_menor(filaDeAtendimento));
 }
